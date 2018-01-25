d267d1cfa5f193fb5294f5272303b191cbb505f6
Kelly Freet <kelly.freet13@yahoo.com>
2018-01-24 22:41:24 -0800

    partially completed class iteration

--- grade_crawl.py
+++ grade_crawl.py
@@ -1,19 +1,23 @@
 from selenium import webdriver
 import time
+import re
 
+NUM_CLASSES = 5
 
 # eventually figure out how to safely store login data
 url = 'https://bbcsulb.desire2learn.com/d2l/login'
 chrome_path = 'C:/Users/Kelly/Desktop/webcrawler/chromedriver_win32/chromedriver'
 
+
 # get username and password
-sid = input('Enter your student id')
-pw = input('Enter your password')
+sid = input('Enter your student id: ')
+pw = input('Enter your password: ')
 
 # create browser
 browser = webdriver.Chrome(chrome_path)
 browser.get(url)
 time.sleep(4)
+home_url = browser.current_url
 
 # get page data
 user = browser.find_element_by_id('userName')
@@ -32,24 +36,30 @@ view_all_courses = browser.find_element_by_id('viewAllCourses')
 view_all_courses.click()
 time.sleep(5)
 
-# THEA 113
-#thea_113_button= browser.find_element_by_id('d2l-course-tile-anchor')
-#thea_113_button.click()
-#time.sleep(3)
-
-################################################
-# I left off trying to click each of the classes
-# but was having trouble finding the right
-# element to click
-################################################
-
-# idea: try just getting the url instead
-
-# get all a tags
+# get all a tags for my classes
 classes = browser.find_elements_by_xpath('//*[@id="d2l-course-tile-anchor"]')
-for link in classes:
-    class_href = link.get_attribute('href')
+for i in range(NUM_CLASSES):
+    curr_url = browser.current_url
+    print('Current url: '+curr_url)
+
+    class_href = classes[i].get_attribute('href')
+
+    # get class name
+    class_name_short = classes[i].text.split(' ', 2)
+    print(class_name_short)
     print(class_href)
-    print(link.text)
+
+    #################################################
+    # There is a problem where the browser doesn't
+    # recognize the element I got after clicking
+    # viewAllCourses. Figure out how to find the
+    # correct elements from the home page, I'm
+    # guessing this should fix the issue.
+    #################################################
+
+    browser.get(class_href)
+    time.sleep(4)
+    browser.back()
+    print('after clicking back, url is: '+browser.current_url)
 
 browser.close()
