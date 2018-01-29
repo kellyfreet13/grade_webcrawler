c44d8da38c7007712e5497f79afe3cb3d32d2d02
Kelly Freet <kelly.freet13@yahoo.com>
2018-01-28 22:11:50 -0800

    just loading some libraries

---
+++ graph_visualizer.py
@@ -0,0 +1,8 @@
+# libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# let's see if git bash takes the file at the time
+# of the $ git add whatever.py or at the time of
+# the push.
