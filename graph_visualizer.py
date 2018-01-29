5c7894b764cb9e0f4664ae602a6c444ad409866c
Kelly Freet <kelly.freet13@yahoo.com>
2018-01-28 22:47:36 -0800

    getting used to plotting data

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# let's see if git bash takes the file at the time
-# of the $ git add whatever.py or at the time of
-# the push.
+# some data
+# Data
+df=pd.DataFrame({'x': range(1,11), 'y1': np.random.randn(10), 'y2': np.random.randn(10)+range(1,11),
+                 'y3': np.random.randn(10)+range(11,21)})
+
+# multiple line plot
+# will eventually contain different grade sections
+plt.plot('x', 'y1', data=df, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
+plt.plot('x', 'y2', data=df, marker='', color='olive', linewidth=2)
+plt.plot('x', 'y3', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="toto")
+plt.legend()
+plt.show()
